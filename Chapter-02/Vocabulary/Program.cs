4e822784a550f053a6af50627bb4eea5940b2bd1
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-25 01:51:04 -0300

    Using reflections to count methods.

--- Chapter-02/Vocabulary/Program.cs
+++ Chapter-02/Vocabulary/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-#error version
+﻿using System.Data;
+using System.Reflection;
+
+//Declare some unused variables using types
+//in additional assemblies
+DataSet ds;
+HttpClient client; 
+
+
+Assembly? myApp = Assembly.GetEntryAssembly();
+
+//quit the app
+if (myApp == null)
+    return;
+
+//loop through the assemblies that my app references
+foreach (var assemblyName in myApp.GetReferencedAssemblies())
+{
+    //Load the assemblies that my app references
+    var assembly = Assembly.Load(assemblyName);
+
+    //Declare a variable to count the number of methods
+    int methodCount = 0;
+
+    //Loop through all the types in the assembly
+    foreach (var definedTypes in assembly.DefinedTypes)
+    {
+        //Add up the counts of methods
+        methodCount += definedTypes.GetMethods().Count();
+    }
+
+    //output the count of types and their methods
+    Console.WriteLine($"{assembly.DefinedTypes:N0} types with {methodCount:N0} methods in {assemblyName.Name:N0} ");
+}
+

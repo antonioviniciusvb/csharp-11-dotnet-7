205014c5cc98abe0d9f1d08cfe241f4620ca89bc
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-25 04:01:01 -0300

    using format strings

---
+++ Formatting/Program.cs
@@ -0,0 +1,29 @@
+﻿int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+Console.WriteLine(
+format: "{0} apples cost {1:C}",
+arg0: numberOfApples,
+arg1: pricePerApple * numberOfApples);
+
+string formatted = string.Format(
+format: "{0} apples cost {1:C}",
+arg0: numberOfApples,
+arg1: pricePerApple * numberOfApples);
+//WriteToFile(formatted); // writes the string into a file
+
+//Interpolated strings -- Recommended
+Console.WriteLine($"{numberOfApples} apples cost {pricePerApple *
+numberOfApples:C}");
+
+
+//Formatings
+string applesText = "Apples";
+int applesCount = 1234;
+
+string bananasText = "Bananas";
+int bananasCount = 56789;
+
+Console.WriteLine($"{"Name",-10} {"Count",6}");
+Console.WriteLine($"{applesText, -10} {applesCount,6:N0}");
+Console.WriteLine($"{bananasText,-10} {bananasCount,6:N0}");
--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,3 +10,17 @@ XmlDocument xml2 = new XmlDocument(); // all C# versions
 // the second statement
 var file1 = File.CreateText("something1.txt");
 StreamWriter file2 = File.CreateText("something2.txt");
+
+XmlDocument xml3 = new(); // target-typed new in C# 9 or later
+
+
+//Target-typed new
+
+// In Program.cs
+Person kim = new();
+kim.BirthDate = new(1967, 12, 26); // instead of: new DateTime(1967, 12, 26)
+// In a separate Person.cs file or at the bottom of Program.cs
+class Person
+{
+    public DateTime BirthDate;
+}

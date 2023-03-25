81e6c63d37c0af154406b03cd28fb5066733be8f
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-25 04:21:32 -0300

    Testing Handling Platforms

---
+++ HandlingPlatforms/Program.cs
@@ -0,0 +1,19 @@
+﻿if (OperatingSystem.IsWindows())
+{
+    // execute code that only works on Windows
+
+    Console.WriteLine("Windows");
+}
+else if (OperatingSystem.IsWindowsVersionAtLeast(major: 10))
+{
+    // execute code that only works on Windows 10 or later
+    Console.WriteLine("win 10");
+}
+else if (OperatingSystem.IsIOSVersionAtLeast(major: 14, minor: 5))
+{
+    // execute code that only works on iOS 14.5 or later
+}
+else if (OperatingSystem.IsBrowser())
+{
+    // execute code that only works in the browser with Blazor
+}

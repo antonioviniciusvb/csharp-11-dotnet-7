65f56313778495c5bb728eaf64dd2b5d26727277
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-25 03:34:33 -0300

    using variables

---
+++ Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+
+// good use of var because it avoids the repeated type
+// as shown in the more verbose second statement
+var xml1 = new XmlDocument(); // C# 3 and later
+XmlDocument xml2 = new XmlDocument(); // all C# versions
+
+// bad use of var because we cannot tell the type, so we
+// should use a specific type declaration as shown in
+// the second statement
+var file1 = File.CreateText("something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");

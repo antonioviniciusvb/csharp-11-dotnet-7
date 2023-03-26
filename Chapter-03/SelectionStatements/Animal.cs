87bfcade362e41eb0c8bdd94cc4f9259ba2826f0
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-26 03:08:11 -0300

    Using switch lambda expressions.

---
+++ Chapter-03/SelectionStatements/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatements
+{
+    public class Animal // This is the base type for all animals.
+    {
+        public string? Name;
+        public DateTime Born;
+        public byte Legs;
+    }
+    public class Cat : Animal // This is a subtype of animal.
+    {
+        public bool IsDomestic;
+    }
+    public class Spider : Animal // This is another subtype of animal
+    {
+        public bool IsPoisonous;
+    }
+}

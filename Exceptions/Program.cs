53cf6603e2b82c8f6429e213b6b7adbb614c967e
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-26 04:12:21 -0300

    Using try/catch with filters.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿Write("Enter an amount: ");
+string amount = ReadLine()!;
+if (string.IsNullOrEmpty(amount)) return;
+try
+{
+    decimal amountValue = decimal.Parse(amount);
+    WriteLine($"Amount formatted as currency: {amountValue:C}");
+}
+catch (FormatException) when (amount.Contains("$"))
+{
+    WriteLine("Amounts cannot use the dollar sign!");
+}
+catch (FormatException)
+{
+    WriteLine("Amounts must only contain digits!");
+}

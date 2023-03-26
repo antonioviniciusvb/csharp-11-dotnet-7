23c7cf1a3c3e3ec9932fdbed4666cec970cce084
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-26 05:10:14 -0300

    Factorial function implementation

--- WritingFunctions/Program.Functions.cs
+++ WritingFunctions/Program.Functions.cs
@@ -5,9 +5,27 @@
     {
         WriteLine($"TimesTable: {number}");
 
-        for (int i = 1; i <= 10; i++) 
+        for (int i = 1; i <= 10; i++)
         {
-            Console.WriteLine($"{number, -2} X {i, 2} ={number * i, 3}");
+            Console.WriteLine($"{number,-2} X {i,2} ={number * i,3}");
+        }
+    }
+
+    static int Factorial(int number)
+    {
+
+        if (number < 0)
+        {
+            throw new ArgumentException(message: "The factorial function is defined for non-negative" +
+                $"integers only. Input {number}", paramName: nameof(number));
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return number * Factorial(number - 1);
         }
     }
 }
--- WritingFunctions/Program.cs
+++ WritingFunctions/Program.cs
@@ -1,2 +1,5 @@
 ﻿TimesTable(7);
+
+int factorial = Factorial(10);
+WriteLine($"10! = {factorial:N0}");
 ReadKey();

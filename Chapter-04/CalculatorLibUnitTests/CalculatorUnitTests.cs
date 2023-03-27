eb6e4fecd955829325795eebab0201a509079aec
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-27 14:59:36 -0300

    Unit tests created

--- Chapter-04/CalculatorLibUnitTests/CalculatorUnitTests.cs
+++ Chapter-04/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -1,3 +1,5 @@
+using CalculatorLib;
+
 namespace CalculatorLibUnitTests
 {
     public class CalculatorUnitTests
@@ -8,8 +10,30 @@ namespace CalculatorLibUnitTests
             //arrange
             double a = 2;
             double b = 2;
+            double expected = 4;
+            Calculator calc = new();
 
-            Calculator.Add(a, b);
+            //act
+            double actual = calc.Add(a, b);
+
+            //assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAdding2And3()
+        {
+            //arrange
+            double a = 2;
+            double b = 3;
+            double expected = 5;
+            Calculator calc = new();
+
+            //act
+            double actual = calc.Add(a, b);
+
+            //assert 
+            Assert.Equal(expected, actual);
         }
     }
 }

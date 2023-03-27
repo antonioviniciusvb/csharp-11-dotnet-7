1c50617a3f56f8c3a932085c0f98c776084566f3
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-27 00:44:29 -0300

    Fibonacci with imperative and functional functions.

---
+++ FibFunctional/Program.cs
@@ -0,0 +1,19 @@
+﻿WriteLine("Fibonacci Functional");
+RunFibFunctional();
+
+static int FibFunctional(int term) =>
+    term switch
+    {
+        1 => 0,
+        2 => 1,
+        > 2 => FibFunctional(term - 1) + FibFunctional(term - 2),
+        _ => 0
+    };
+
+static void RunFibFunctional()
+{
+    for (int i = 1; i <= 30; i++)
+    {
+        WriteLine($"term {i, -5:N0} {FibFunctional(i), 10:N0}");
+    }
+}
---
+++ FibImperative/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Finbonacci Imperative");
+RunFibImperative();

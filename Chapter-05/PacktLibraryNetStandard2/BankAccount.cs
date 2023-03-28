4dacbf1a08f9e85b6431c8c6c6f3d08d19fa4e55
antonioviniciusvb <antonioviniciusvb@outlook.com>
2023-03-27 21:51:42 -0300

    Using static class members.

---
+++ Chapter-05/PacktLibraryNetStandard2/BankAccount.cs
@@ -0,0 +1,7 @@
+﻿namespace PacktShare;
+public class BankAccount
+{
+    public string? AccountName { get; set; } //instance member
+    public decimal Balance { get; set; } // instance member
+    public static decimal InterestRate { get; set; } //shared member
+}
--- Chapter-05/PeopleApp/Program.cs
+++ Chapter-05/PeopleApp/Program.cs
@@ -1,26 +1,54 @@
 ﻿using Packt.Shared;
+using PacktShare;
 using static Pack.Shared.WondersOfTheAncientWorld;
 
-Person bob = new();
-WriteLine(bob.ToString());
+#region Sample Class, Enum
+//Person bob = new();
+//WriteLine(bob.ToString());
 
-bob.Name = "Bob Smith";
-bob.BucketList = GreatPyramidOfGiza|ColossusOfRhodes;
-bob.DateOfBirth = new DateTime(1965, 12, 22); // C# 1.0 or later
+//bob.Name = "Bob Smith";
+//bob.BucketList = GreatPyramidOfGiza|ColossusOfRhodes;
+//bob.DateOfBirth = new DateTime(1965, 12, 22); // C# 1.0 or later
 
-bob.Children.Add(new Person { Name = "Alfred" });
-bob.Children.Add(new Person { Name = "Zoe" });
+//bob.Children.Add(new Person { Name = "Alfred" });
+//bob.Children.Add(new Person { Name = "Zoe" });
 
-WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}, Favorite: {2}",
-arg0: bob.Name,
-arg1: bob.DateOfBirth,
-arg2: bob.BucketList);
+//WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}, Favorite: {2}",
+//arg0: bob.Name,
+//arg1: bob.DateOfBirth,
+//arg2: bob.BucketList);
 
-WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+//WriteLine($"{bob.Name} has {bob.Children.Count} children:");
 
-foreach (Person person in bob.Children)
+//foreach (Person person in bob.Children)
+//{
+//    WriteLine($"{person.Name}");
+//}
+
+#endregion
+
+#region Sample Static Members
+
+BankAccount.InterestRate = 0.012M; //store a shared value
+
+BankAccount jonesAccount = new()
 {
-    WriteLine($"{person.Name}");
-}
+    AccountName = "Mrs. Jones",
+    Balance = 2400
+};
+
+WriteLine($"{jonesAccount.AccountName} earned " +
+        $"{jonesAccount.Balance * BankAccount.InterestRate:C}");
+
+BankAccount gerrierAccount = new()
+{
+    AccountName = "Mrs. Gerrier",
+    Balance = 98
+};
+
+WriteLine($"{gerrierAccount.AccountName} earned " +
+        $"{gerrierAccount.Balance * BankAccount.InterestRate:C}");
+
+#endregion
 
 ReadKey();
